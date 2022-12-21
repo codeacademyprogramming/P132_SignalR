b7d15fec4c3687435f47ecb05323020e4b036bef
eaghayev <eaghayevv@gmail.com>
2022-12-21 16:34:46 +0400

    Projects added

---
+++ SignalR/ChatApp/wwwroot/js/chat.js
@@ -0,0 +1,31 @@
+﻿
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    console.log(connection)
+    console.log(connection.connectionId)
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+document.getElementById("sendButton").addEventListener("click", function (e) {
+
+    var name = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", name, message);
+})
+
+
+connection.on("ReceiveMessage", function (name, message) {
+
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${name} says ${message}`;
+})
+
